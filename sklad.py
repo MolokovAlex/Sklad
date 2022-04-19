93a8a50c9848d0f5efd31bb4e870dde816a9f8c9
MolokovAlex <milky77@mail.ru>
2022-04-19 17:38:52 +0300

    version 0.0.4 - функции импорта/экспорта в pickle файл

--- sklad.py
+++ sklad.py
@@ -3,6 +3,7 @@
 # coding: utf-8
 # version 0.0.2 - создаю класс Component и генератор уникального номера
 # version 0.0.3 - функции создания резрвного файла
+# version 0.0.4 - функции импорта/экспорта в pickle файл
 
 # надо: 
 # ОК- делать копию того файла который открываем и открывать копию, при закрытии файла - обратная замена. Те.в папке будут два файла - исходный и измененный
@@ -10,14 +11,17 @@
 # - сохранять и считывать в собственном фомате в файл
 # ОК- генерировать уникальный номер компонента в зависимости от даты, времени до милисекунды
 
+from re import X
 import win32com.client
 import time
 import shutil
-
+import pickle
+import sys
+import os
 
 class Component:
     def __init__(self, name, code, 
-                group, sub_group_level1, sub_group_level2, sub_group_level3, sub_group_level4, sub_group_level5, 
+                group='', sub_group_level1='', sub_group_level2='', sub_group_level3='', sub_group_level4='', sub_group_level5='', 
                 amount=0, dimencion='шт', articul=''):
         """ Конструктор класса Component
 
@@ -60,6 +64,7 @@ def getCode():
     Ccode -  тип int,  число увелививающееся на 1 каждые 100 мс
     """
     CCode = ''
+    time.sleep(0.2)         # задержка генерации кода в 0.2 сек
     Utime = time.time()
     #  ограничим число, чтобы оно не было таким большим
     Utime = int(round((Utime-1640000000),1)*10)
@@ -76,30 +81,96 @@ def SeparationFileName (nameFile:str) -> list:
     list_name = []
     name = ''
     ext = ''
-    for x in range (len(nameFile)-1,0,-1):
+    for x in range (len(nameFile)-1,0,-1):      #  поиск расширения
         if nameFile[x] != ".":
             ext = nameFile[x] + ext
         else:
             x_stop = x
             break
-    for y in range (0,x_stop,1):
+    for y in range (0,x_stop,1):                # поиск имени
         name = name + nameFile[y]
     list_name.append(name)
     list_name.append(ext)
     return list_name
 
-def ArchFileCopy(nameFileOrigin:str):
+def ArchFileCopy(nameFileOrigin:str)-> str:
     """
     функция делает копию исходного файла, добавляя к имени в конце "V1"
+    вход:
+    nameFileOrigin:str - например, "test.xlsx"
+    выход:
+    создание копии и 
+    nameArchFile - имя созданного файла, например "testV1.xlsx"
     """
     nameOrigin = []
     nameOrigin = SeparationFileName(nameFileOrigin)
-    archFile = nameOrigin[0] + 'V1' + '.' + nameOrigin[1]
-    shutil.copy2(nameFileOrigin, archFile)
-    return archFile
+    nameArchFile = nameOrigin[0] + 'V1' + '.' + nameOrigin[1]
+    shutil.copy2(nameFileOrigin, nameArchFile)
+    return nameArchFile
+
+def FillingDBComponent():
+    """
+     функция наполнения базы данных класса Component
+    """
+
+
+    return None
+
+def SaveDBComponentInFileXLS():
+    """
+    функция экспорта базы  DBComponent в файл xls
+    """
+    return None
+
+def SaveDBComponentInFilePickle(fileName: str):
+    """
+    функция экспорта базы  DBComponent в файл упаковкой pickle
+    """
+    global DBComponent
+    with open(fileName) as dbfile:
+        dbfile = open(fileName, "wb")
+        pickle.dump(DBComponent, dbfile, 4)
+    dbfile.close()
+    print ("OK file save")
+    return None
+
+def LoadDBComponentInFilePickle(fileName: str):
+    """
+    функция импорта базы  DBComponent из файла упаковкой pickle
+    """
+    global DBComponent
+    with open(fileName) as dbfile:
+        dbfile = open(fileName, "rb")
+        DBComponent = pickle.load(dbfile)
+    dbfile.close()
+    print ("OK file load")
+    return None
+
 
 nameFile = "ex1.xlsx"
-print (getCode())
-print (SeparationFileName(nameFile))
+fileNamePickle = 'DBComponents.db'
+DBComponent = []                # БД по наименованию компонентов
+
 print (ArchFileCopy(nameFile))
 
+# тестовое наполнение DBComponent
+baseNameComponent = 'транзистор'
+for x in range (0, 10, 1):
+    nameComponent = baseNameComponent + str(x)
+    obj = Component(nameComponent, getCode())
+    DBComponent.append(obj)
+for x in range (0, 10, 1):
+    print (DBComponent[x].code + '  ' + DBComponent[x].name)
+
+#os.chdir(r'G:\NO_Work\Python\Sklad')       # change dir
+#print(os.getcwd())          # print current directory
+
+SaveDBComponentInFilePickle(fileNamePickle)
+
+
+DBComponent = []
+
+LoadDBComponentInFilePickle(fileNamePickle)
+
+for x in range (0, 10, 1):
+    print (DBComponent[x].code + '  ' + DBComponent[x].name)
